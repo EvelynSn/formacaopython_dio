5d9df85e46c4a84b01017054486e5de98084d1bb
Evelyn Sena <78700147+EvelynSn@users.noreply.github.com>
2024-03-18 17:23:43 -0300

    Add files via upload
    Desafio
    Daenerys é a khaleesi dos Dothraki. Juntamente com Drogon, eles vão atrás do Tyrion, para tentar dominar Westeros. Ela possui, além do seu dragãozinho, um rastreador que mede o nível de energia de qualquer ser vivo. Todos os seres com o nível menor ou igual a 8000, ela considera como se fosse um inseto. Quando passa deste valor, que foi o caso do Drogon, ela se espanta e grita “Mais de 8000”. Baseado nisso, utilize a mesma tecnologia e analise o nível de energia dos seres vivos.
    
    Entrada
    A primeira linha contém um número inteiro C relativo ao número de casos de teste. Em seguida, haverá C linhas, com um número inteiro N (100 <= N <= 100000) relativo ao nível de energia de um ser vivo.

---
+++ code72.py
@@ -0,0 +1,11 @@
+caso_teste = int(input())
+
+for _ in range(caso_teste):
+    bicho = input()
+    
+    if bicho > 8000:
+        print ('Mais de 8000!')
+    
+    else:
+        print('Inseto!')
+
